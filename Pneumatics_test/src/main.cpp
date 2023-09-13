71802d6d4825d269c044e60a8fb0bcf0c61dcef4
jmartin-bancroft <jmartin@bancroftschool.org>
2023-09-13 16:23:10 -0400

    Initial Commit
    Needs to be in git

---
+++ Pneumatics_test/src/main.cpp
@@ -0,0 +1,35 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Jeremy School                                    */
+/*    Created:      Tue Sep 12 2023                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// DigitalOutA          digital_out   A               
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  // if(ButtonB){
+  //   DigitalOutA.set(false);
+  // }else if(ButtonA){
+  //   DigitalOutA.set(true);
+  // }
+  while(1){
+    DigitalOutA.set(true);
+    wait(2000,msec);
+    DigitalOutA.set(false);
+    wait(2,sec);
+  }
+}
